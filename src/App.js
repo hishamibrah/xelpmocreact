1c902759d4cb637c60b0b4123891192b0d2b9901
shamivul <shamivulruvaid@gmail.com>
2020-02-19 23:54:04 +0530

    fixes Routing error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
 import SignUpForm from './pages/SignUpForm';
 import SignInForm from './pages/SignInForm';
 
@@ -8,27 +8,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div className="App">
           <div className="AppAside"></div>
           <div className="AppForm">
             <div className="PageSwitcher">
-                <NavLink to="/signin" activeClassName="PageSwitcherItemActive" className="PageSwitcherItem">Sign In</NavLink>
+                <NavLink to="/sign-in" activeClassName="PageSwitcherItemActive" className="PageSwitcherItem">Sign In</NavLink>
                 <NavLink exact to="/" activeClassName="PageSwitcherItemActive" className="PageSwitcherItem">Sign Up</NavLink>
               </div>
-
               <div className="FormTitle">
-                  <NavLink to="/signin" activeClassName="FormTitleLinkActive" className="FormTitleLink">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitleLink--Active" className="FormTitleLink">Sign Up</NavLink>
+                  <NavLink to="/sign-in" activeClassName="FormTitleLinkActive" className="FormTitleLink">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitleLink--Active" className="FormTitleLink">Sign Up</NavLink>
               </div>
-
-              <Route exact path="/" component={SignUpForm}>
-              </Route>
-              <Route path="/signin" component={SignInForm}>
-              </Route>
+              <Route exact path="/" component={SignUpForm}/>
+              <Route path="/sign-in" component={SignInForm}/>
           </div>
-
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
--- src/pages/calculations/calculation.js
+++ src/pages/calculations/calculation.js
@@ -1,1 +1,30 @@
+import React, { Component } from 'react';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Input } from 'semantic-ui-react';
+import Button from '@material-ui/core/Button';
 
+class Calculation extends Component {
+  render() {
+    return (
+    <div>
+      <Tabs>
+        <TabList>
+          <Tab>Input</Tab>
+          <Tab>Sum and Substraction</Tab>
+          <Tab>Multiplication and Division</Tab>
+        </TabList>
+        <TabPanel>
+          <Button variant="contained" color="primary" component="span">
+            Submit
+          </Button>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+    );
+  }
+}
+export default Calculation;
